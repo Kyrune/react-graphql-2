a632b8f339a1831c32802c78b3fb28f56efadf69
Kyrune <jn-01@centurytel.net>
2020-07-20 10:58:20 -0500

    Import ApolloClient, ApolloProvider & PokemonsContainer components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import { PokemonsContainer } from './containers/PokemonsContainer';
 
 function App() {
   return (
